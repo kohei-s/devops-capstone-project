97465f5d2a25e3528af7f239d22e362d236adbde
kohei-s <kicoyuz@gmail.com>
2023-02-23 15:44:56 -0500

    update created

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -150,3 +150,15 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(len(data), 7)
 
+    def test_update_account(self):
+        """It should Update an existing Account"""
+        test_account = AccountFactory()
+        responce = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(responce.status_code, status.HTTP_201_CREATED)
+
+        new_account = responce.get_json()
+        new_account["name"] = "FOO"
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], "FOO")
