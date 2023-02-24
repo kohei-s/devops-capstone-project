95f6068779a9be30ef9d2ecb0a81b0315e76c9c7
kohei-s <kicoyuz@gmail.com>
2023-02-24 06:51:30 -0500

    completed ci build4

--- service/routes.py
+++ service/routes.py
@@ -115,7 +115,7 @@ def update_accounts(account_id):
 def delete_accounts(account_id):
     """
     Delete an Account
-    This endpoint will delete an Account based on the account_id that is requested 
+    This endpoint will delete an Account based on the account_id that is requested
     """
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
